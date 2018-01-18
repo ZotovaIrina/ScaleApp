d5f70bc254c23de76ae8744725dc03a36e77c6d6
ZotovaIrina <zotovaiv87@gmail.com>
2018-01-18 12:42:41 -0500

    Add X after click on button

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        }
+    }
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
