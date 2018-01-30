879a8cbbbd5b1d430bfb2773a1d06c1625896992
ZotovaIrina <zotovaiv87@gmail.com>
2018-01-30 13:12:57 -0500

    Rename files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
     BrowserRouter as Router
 } from 'react-router-dom';
 import Routes from './routes'
-import NavigationBar from './NavigationBar/NavigationBar.js'
+import NavigationBar from './components/navigationBar/NavigationBar.js'
 
 class App extends React.Component {
     render() {
