b0bb2c12422e4029a1484e675ad81b0eb84fa059
ZotovaIrina <zotovaiv87@gmail.com>
2018-02-03 17:49:46 -0500

    Form validation

--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -6,40 +6,93 @@ class LoginPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userName: 'Sofia',
-        }
+            userName: '',
+            errorMessage: '',
+            isDisabled: true,
+        };
+        this.onChange = this.onHandleChange.bind(this);
     }
+
+    errorUserNameShort = 'Username should contain no less than 3 symbols';
+    errorUserNameIsRequired = 'User Name is Required';
 
     submitLogin() {
         storage.setItem('UserName', this.state.userName)
     }
 
-    onHandleChange (event) {
+    onHandleChange(event) {
+        let value = event.target.value;
         this.setState({
-            userName: event.target.value
+            [event.target.name]: value,
         });
+        this.userNameIsValid(value);
+        if (this.userNameIsValid(value).isValid) {
+            this.setState({
+                isDisabled: false,
+                errorMessage: '',
+            });
+        } else {
+            this.setState({
+                isDisabled: true,
+            });
+        }
     }
 
+    onHandleBlur(event) {
+        if (this.userNameIsValid(event.target.value).isValid) {
+            this.setState({
+                isDisabled: false,
+            });
+        } else {
+            this.setState({
+                isDisabled: true,
+                errorMessage: <p>{this.userNameIsValid(event.target.value).error}</p>
+            });
+        }
+    }
+
+    userNameIsValid(value) {
+        if(value.length >= 3) {
+            return {isValid: true};
+        } else if(!value) {
+            return {
+                error: this.errorUserNameIsRequired,
+                isValid: false,
+            };
+        } else {
+            return {
+                error: this.errorUserNameShort,
+                isValid: false,
+            };
+        }
+
+    }
 
     render() {
         return (
             <div>
                 <h1>Hello New User!</h1>
                 <p>Please input login and password</p>
-                <label>User Name:
-                    <input name="userName"
-                           type="text"
-                           value={this.state.userName}
-                           onChange={(event) => this.onHandleChange(event)} />
-                </label>
-                <label>Password:
-                    <input name="password"
-                           type = "password"/>
-                </label>
-                <button type="Submit"
-                        onClick={() => this.submitLogin()}>
-                    Submit
-                </button>
+                <form>
+                    <label>User Name:
+                        <input name="userName"
+                               type="text"
+                               value={this.state.userName}
+                               onChange={this.onChange}
+                               placeholder="User Name"
+                               onBlur={(event) => this.onHandleBlur(event)}/>
+                    </label>
+                    {this.state.errorMessage}
+                    <label>Password:
+                        <input name="password"
+                               type="password"/>
+                    </label>
+                    <button type="Submit"
+                            disabled={this.state.isDisabled}
+                            onClick={() => this.submitLogin()}>
+                        Submit
+                    </button>
+                </form>
             </div>
         )
     }
