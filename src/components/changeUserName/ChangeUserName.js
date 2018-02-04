b0a75ff731738bd6357cbab5a4dff79a881e9583
ZotovaIrina <zotovaiv87@gmail.com>
2018-02-04 15:48:51 -0500

    ChangeUserName component

--- src/components/changeUserName/ChangeUserName.js
+++ src/components/changeUserName/ChangeUserName.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {setName} from '../../store/actions/userActions'
 
 class ChangeUserName extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            userName: this.props.userName,
+        };
+        this.onChange = this.onHandleChange.bind(this);
+    }
+
+    onHandleChange(event) {
+        console.log('event.target.value');
+        this.setState({
+            userName: event.target.value
+        });
+
+    }
+
+    changeUserNameFunction(newName) {
+        this.props.setName(newName);
     }
 
     render() {
@@ -13,13 +31,29 @@ class ChangeUserName extends React.Component {
                         <input name="userName"
                                placeholder="Input new user name"
                                type="text"
-                               value={this.props.userName}/>
+                               onChange={this.onChange}
+                               value={this.state.userName}/>
                     </label>
-                    <button type="submit" onClick={() => this.props.changeUserName()}>Change user name</button>
+                    <button type="submit"
+                            onClick={() => this.changeUserNameFunction(this.state.userName)}>
+                        Change user name
+                    </button>
                 </form>
             </div>
         )
     }
 }
 
-export default ChangeUserName;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setName: (name) => {
+            dispatch(setName(name))
+        }
+    }
+};
+export default connect(mapStateToProps, mapDispatchToProps)(ChangeUserName);
