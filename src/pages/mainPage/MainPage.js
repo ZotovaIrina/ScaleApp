0b0c845f42c2bbf8ab9e60c7231eaf35d59d4117
ZotovaIrina <zotovaiv87@gmail.com>
2018-02-04 15:47:46 -0500

    Change userName in ChangeUserName component

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -6,13 +6,42 @@ import ChangeUserName from '../../components/changeUserName/ChangeUserName'
 
 class MainPage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            components: [],
+            showChangeNameButton: true,
+        };
+    }
+
+    changeUserName = <ChangeUserName key="ChangeUserName"
+                                     userName={this.props.user.userName}/>;
+
+
+    addNewComponent(newComponent) {
+        this.setState({
+            components: [...this.state.components, newComponent]
+        });
+    }
+
+    onClickChangeUserName() {
+        this.addNewComponent(this.changeUserName);
+        this.setState({
+            showChangeNameButton: false,
+        });
+    }
+
     render() {
         return (
-                <div>
-                    <h1>Main Page</h1>
-                    <p>Hello {this.props.user.userName}</p>
-                    <ChangeUserName />
-                </div>
+            <div>
+                <h1>Main Page</h1>
+                <p>Hello {this.props.user.userName}</p>
+                <button className={this.state.showChangeNameButton ? '' : 'd-none'}
+                    onClick={() => this.onClickChangeUserName()}>
+                    Change User Name
+                </button>
+                {this.state.components}
+            </div>
         )
     }
 }
